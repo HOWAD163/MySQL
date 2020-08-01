b3398f311bddc927797992bc2662b87fb6ea24d5
regrets1024 <62733388+regrets1024@users.noreply.github.com>
2020-08-01 16:41:17 +0800

    Update 存储过程游标模板.sql

--- 存储过程游标模板.sql
+++ 存储过程游标模板.sql
@@ -1,1 +1,44 @@
+# 将语句的结束符号从分号; 临时改为两个$$(可以是自定义)
+delimiter $$
 
+CREATE DEFINER = `root` @`localhost` PROCEDURE `procedure_test` ( IN p_tag VARCHAR ( 255 ) ) COMMENT '游标模板' BEGIN
+
+DECLARE
+	p_court_id INT DEFAULT 0;
+	
+DECLARE
+	done INT DEFAULT 0;
+DECLARE
+	l_cursor CURSOR FOR SELECT
+	court_id 
+FROM
+	courts 
+WHERE
+	tag IN ( p_tag ) 
+ORDER BY
+	court_id;
+	
+DECLARE
+	CONTINUE HANDLER FOR SQLSTATE '02000' 
+	SET done = 1;
+	
+OPEN l_cursor;
+REPEAT
+	FETCH l_cursor INTO p_court_id;
+IF
+	NOT done THEN
+	
+	
+SELECT
+	p_court_id;
+
+
+END IF;
+UNTIL done 
+END REPEAT;
+CLOSE l_cursor;
+
+END$$
+
+# 将语句的结束符号恢复为分号
+delimiter ;
