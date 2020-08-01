eb1ef78d2049b3c4971136d7e54d9199de07f200
regrets1024 <62733388+regrets1024@users.noreply.github.com>
2020-08-01 15:47:32 +0800

    Update 查询导出Excel.sql

--- 查询导出Excel.sql
+++ 查询导出Excel.sql
@@ -1,8 +1,8 @@
 SELECT
-	user_name 
-  INTO OUTFILE '/mnt/mysql5.7/casecheck_bak/userinfo_bak.xls' 
-  CHARACTER SET gbk 
+    user_name 
+    INTO OUTFILE '/mnt/mysql5.7/casecheck_bak/userinfo_bak.xls' 
+    CHARACTER SET gbk 
 FROM
-	userinfo 
+    userinfo 
 WHERE
-	id > 1;
+    id > 1;
